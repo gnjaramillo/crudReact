d4b4f42c931940de3b1657a29f8c25e8ca5a0787
gnjaramillo <gnjaramillo1986@gmail.com>
2024-02-19 11:36:20 -0500

    cambios css y eliminación boton new que no se estaba usando

--- src/componentes/todo.jsx
+++ src/componentes/todo.jsx
@@ -1,30 +1,37 @@
 // Todo.jsx
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
 
+// Componente funcional Todo que recibe props item, onUpdate y onDelete
 function Todo({ item, onUpdate, onDelete }) {
+    // Estado para controlar si se está editando el todo o no
     const [isEdit, setIsEdit] = useState(false);
 
+    // Componente interno para el formulario de edición del todo
     function FormEdit() {
+        // Estados para controlar los nuevos valores del título y descripción
         const [newValue, setNewValue] = useState(item.title);
         const [newDesc, setNewDesc] = useState(item.description);
 
+        // Manejador para la presentación del formulario de edición
         function handleSubmit(e) {
-            e.preventDefault();
-            onUpdate(item.id, newValue, newDesc);
-            setIsEdit(false);
+            e.preventDefault(); // Prevenir comportamiento por defecto del formulario
+            onUpdate(item.id, newValue, newDesc); // Llamar a la función onUpdate para actualizar el todo
+            setIsEdit(false); // Cambiar el estado de isEdit a false para volver a la visualización del todo
         }
 
+        // Manejador para el cambio en el valor del título
         function handleChange(e) {
             const value = e.target.value;
             setNewValue(value);
         }
 
+        // Manejador para el cambio en el valor de la descripción
         function handleChangeDesc(e) {
             const value = e.target.value;
             setNewDesc(value);
         }
 
+        // Retornar el formulario de edición
         return (
             <form className='todoUpdateForm' onSubmit={handleSubmit}>
                 <input type="text" className='todoInput' onChange={handleChange} value={newValue} />
@@ -34,6 +41,7 @@ function Todo({ item, onUpdate, onDelete }) {
         );
     }
 
+    // Componente interno para la visualización del todo
     function TodoElement() {
         return (
             <section className='todoContent'>
@@ -41,7 +49,6 @@ function Todo({ item, onUpdate, onDelete }) {
                     <div>
                         <h1>Nombre</h1>
                         <span className='todoTitle'>{item?.title}</span>
-
                     </div>
                     <div>
                         <h1>Descripción</h1>
@@ -49,17 +56,17 @@ function Todo({ item, onUpdate, onDelete }) {
                     </div>       
                     <div>
                         <h1>Opciones</h1>
+                        {/* Botón para editar el todo */}
                         <button className='buttonEdit' onClick={() => setIsEdit(true)}>Edit</button>
+                        {/* Botón para eliminar el todo */}
                         <button className='buttonDelete' onClick={(e) => onDelete(item.id)}>Delete</button>
                     </div>
                 </div>
-                <button className='buttonNew' onClick={() => ( <Navigate to="/todo">Redirigir a Todo</Navigate>
-                )}>New</button>
             </section>
         );
     }
-    
 
+    // Retornar el componente Todo, mostrando el formulario de edición si isEdit es true, o la visualización del todo en caso contrario
     return (
         <div className='todo'>
             {isEdit ? <FormEdit /> : <TodoElement />}
@@ -67,6 +74,5 @@ function Todo({ item, onUpdate, onDelete }) {
     );
 }
 
-
-
+// Exportar el componente Todo
 export default Todo;
--- src/componentes/todoApp.jsx
+++ src/componentes/todoApp.jsx
@@ -1,61 +1,68 @@
 // todoApp.jsx
 import React, { useState } from 'react';
-import { NavLink, Navigate } from 'react-router-dom';
-import Todo from './todo'; 
-import "./todoApp.css"
+import { Navigate } from 'react-router-dom'; // Importamos Navigate para la redirección
+import Todo from './todo'; // Importamos el componente Todo
+import "./todoApp.css" // Importamos estilos CSS
 
 const TodoApp = () => {
+    // Definimos los estados para el título, descripción y la lista de todos
     const [title, setTitle] = useState(""); 
     const [description, setDescription] = useState(""); 
     const [todos, setTodos] = useState([]); 
 
+    // Manejador de cambio para el título
     function handleChange(event) {
         const { value } = event.target;
         setTitle(value);
     }
     
+    // Manejador de cambio para la descripción
     function handleChangeDesc(event) {
         const { value } = event.target;
         setDescription(value);
     }
 
+    // Manejador para la presentación del formulario
     function handleSubmit(e) {
-        e.preventDefault();
+        e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
     
+        // Crear un nuevo objeto de todo
         const newTodo = {
-            id: crypto.randomUUID(), //genera id aleatorio
+            id: crypto.randomUUID(), // Genera un ID aleatorio
             title: title,
             description: description,
             completed: false
         }
     
-        const temp = [...todos]; 
-        temp.unshift(newTodo); 
-        setTodos(temp); 
+        const temp = [...todos]; // Crear una copia de la lista actual de todos
+        temp.unshift(newTodo); // Agregar el nuevo todo al principio de la lista
+        setTodos(temp); // Actualizar el estado de la lista de todos con la nueva lista
     
-        setTitle("");
-        setDescription("");
+        setTitle(""); // Limpiar el título después de la presentación del formulario
+        setDescription(""); // Limpiar la descripción después de la presentación del formulario
 
-        // Redirección a Todo
-        <Navigate to="/todo">Redirigir a Todo</Navigate>
+        
     }
 
+    // Manejador para la actualización de un todo
     function handleUpdate(id, newValue, newDesc) {
-        const temp = [...todos];
-        const item = temp.find(item => item.id === id);
-        item.title = newValue;
-        item.description = newDesc;
-        setTodos(temp);
+        const temp = [...todos]; // Crear una copia de la lista actual de todos
+        const item = temp.find(item => item.id === id); // Encontrar el todo correspondiente al ID
+        item.title = newValue; // Actualizar el título del todo
+        item.description = newDesc; // Actualizar la descripción del todo
+        setTodos(temp); // Actualizar el estado de la lista de todos con la nueva lista
     }
 
+    // Manejador para eliminar un todo
     function handleDelete(id) {
-        const temp = todos.filter(item => item.id !== id); 
-        setTodos(temp);
+        const temp = todos.filter(item => item.id !== id); // Filtrar los todos y excluir el que coincide con el ID
+        setTodos(temp); // Actualizar el estado de la lista de todos con la nueva lista
     }
     
     return (
         <div className='todoContainer'>
             <h1 className='tituloTodoContainer'>Area del conocimiento</h1>
+            {/* Formulario para crear un nuevo todo */}
             <form className="todoCreateForm" onSubmit={handleSubmit}>
                 <input placeholder='Area del Conocimiento' onChange={handleChange} className='todoInput' value={title} />  
                 <input placeholder='Descripcion' onChange={handleChangeDesc} className='todoInput' value={description} />
@@ -64,6 +71,7 @@ const TodoApp = () => {
                 <button type='button' className="buttonCancel" onClick={() => {setTitle(""); setDescription("")}} >CANCEL</button>
             </form>
 
+            {/* Contenedor para mostrar la lista de todos */}
             <div className='todosContainer'> 
                 {todos.map((item) => ( 
                     <Todo key={item.id} item={item} onUpdate={handleUpdate} onDelete={handleDelete}/>
@@ -74,3 +82,7 @@ const TodoApp = () => {
 }
 
 export default TodoApp;
+
+
+//Pasamos el objeto item completo 
+// como prop al componente Todo, para que tenga acceso a todas las propiedades de ese elemento individual.
